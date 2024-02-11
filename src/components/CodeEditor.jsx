8c7d29853c0a34d607e9ec03ce98fec3aeb5abf2
Pritam Das <dpritam28577@gamil.com>
2024-02-11 10:55:25 +0530

    Add useState hook to CodeEditor and import it in main.jsx

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import { Box } from "@chakra-ui/react";
 import Editor from "@monaco-editor/react";
 const CodeEditor = () => {
+  const [code, setCode] = useState("");
   return (
     <Box>
       <Editor
         height="90vh"
+        theme="vs-dark"
         defaultLanguage="javascript"
         defaultValue="//try you code here"
+        value={code}
+        onChange={(value) => setCode(value)}
       />
     </Box>
   );
