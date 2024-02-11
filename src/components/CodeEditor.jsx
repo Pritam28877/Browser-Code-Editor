7855d66a4fc79aa4c33337efb96be458e918b1a0
Pritam Das <dpritam28577@gamil.com>
2024-02-11 11:22:51 +0530

    Add language selection and editor focus

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,8 +1,14 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Box } from "@chakra-ui/react";
 import Editor from "@monaco-editor/react";
 const CodeEditor = () => {
   const [code, setCode] = useState("");
+  const [language, setLanguage] = useState("javascript");
+  const editorRef = useRef();
+  const onMount = (editor) => {
+    editorRef.current = editor;
+    editor.focus();
+  };
   return (
     <Box>
       <Editor
