376c4c5b4fa68b10f6ca078afc41efbc0df9c51e
Pritam Das <dpritam28577@gamil.com>
2024-02-11 10:27:53 +0530

    Refactor App component and add CodeEditor component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
-import {Box} from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react";
+import CodeEditor from "./components/CodeEditor";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>
-  <Box>
-    
-  </Box>
-  </>;
+  return (
+    <>
+      <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
+        <CodeEditor />
+      </Box>
+    </>
+  );
 }
 
 export default App;
